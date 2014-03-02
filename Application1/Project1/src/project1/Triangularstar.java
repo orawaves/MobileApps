587ae1dd85e111bdd41595eb6a38940f25c556f1
orawaves <orawaves123@gmail.com>
2014-03-02 15:09:31 -0800

    test 3

--- Application1/Project1/src/project1/Triangularstar.java
+++ Application1/Project1/src/project1/Triangularstar.java
@@ -2,16 +2,16 @@ package project1;
 
 public class Triangularstar {
     public static void main(String[] args) {
-      
-      
+
+
       int i,j;
-      
+
     for(i=1;i<=5;i++){
                         for(j=1;j=i;j++){
                                            System.out.println("*");
                                          }
-        System.out.println("test2");
+        System.out.println("test3");
       }
-                      
+
     }
 }
